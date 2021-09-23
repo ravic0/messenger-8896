81d4aa4fd3dcaf40fe1d0373c6ce5ee5755bb1cb
ravic0 <ravnit.sehgal@gmail.com>
2021-09-22 21:12:48 -0400

    Migrated db credentials

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const Sequelize = require("sequelize");
 
 const db = new Sequelize(
   process.env.DATABASE_URL ||
-    "postgres://postgres:admin@localhost:5432/messenger",
+  "postgres://localhost:5432/messenger",
   {
     logging: false,
   }
